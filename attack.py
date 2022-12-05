db9f8a3494bc6a1e2721992b52c9c5d9e1a13fa9
Ignacy Makowski <78569836+makowskiignacy@users.noreply.github.com>
2022-12-05 18:41:44 +0100

    ARTAttack development
    code restructurization , adding support for Pytorch and Sklearn to ARTAttack class

--- attack.py
+++ attack.py
@@ -1,11 +1,9 @@
-from tensorflow import keras
-import tensorflow as tf
+import torch
+import sklearn
 
-import art
-from art.attacks.evasion import FastGradientMethod
-from art.estimators.classification import KerasClassifier
+from tensorflow import keras
+from art.estimators.classification import KerasClassifier, PyTorchClassifier, SklearnClassifier, TensorFlowV2Classifier
 from art.attacks.evasion import AdversarialPatch
-from art.utils import load_mnist
 
 
 class Attack:
@@ -21,84 +19,84 @@ class ARTAttack(Attack):
     def to_unified_format(data_from_attack):
         pass
 
-    # x(ndarray)–An array with the original input images of shape NHWC or NCHW or input videos of shape NFHWC or NFCHW.
-    # y – An array with the original true labels.
-    # mask (np.ndarray) – A boolean array of shape equal to the shape of a single samples (1, H, W)
-    # or the shape of x (N, H, W) without their channel dimensions.
-    # Any features for which the mask is True can be the center location of the patch during sampling.
-    # reset_patch (bool) – If True reset patch to initial values of mean of minimal and maximal clip value,
-    # else if False (default) restart from previous patch values created by previous call to generate
-    # or mean of minimal and maximal clip value if first call to generate.
     def __init__(self, args):
         super().__init__(args)
 
         self.classifier = None
         self.attack = None
+        self.data = None
 
-        # Pytanie czy data i args powinny być osobnymi bytami,
-
-        # tutaj jest pomieszanie z popląctaniem x=data pozostałe to parametry
-
-        if "y" in args:
-            self.y = args["rotation_max"]
-        else:
-            self.y = None
-
+        # mask (np.ndarray) – A boolean array of shape equal to the shape of a single samples (1, H, W)
+        # or the shape of x (N, H, W) without their channel dimensions.
+        # Any features for which the mask is True can be the center location of the patch during sampling.
         if "mask" in args:
             self.mask = args["mask"]
         else:
             self.mask = None
 
+        # reset_patch (bool) – If True reset patch to initial values of mean of minimal and maximal clip value,
+        # else if False (default) restart from previous patch values created by previous call to generate
+        # or mean of minimal and maximal clip value if first call to generate.
         if "reset_patch" in args:
             self.reset_patch = args["reset_patch"]
         else:
             self.reset_patch = False
 
+        if "input_shape" in args:
+            self.input_shape = args["input_shape"]
+        else:
+            self.input_shape = None
+
+        if "loss" in args:
+            self.loss = args["loss"]
+        else:
+            self.loss = None
+
+        if "nb_classes" in args:
+            self.nb_classes = args["nb_classes"]
+        else:
+            self.nb_classes = None
+
     def conduct(self, model, data):
-        if self.y is not None and self.mask is not None:
-            return ARTAttack.to_unified_format(self.attack.generate(x=data, y=self.y, mask=self.mask, reset_patch=self.reset_patch))
+        if self.data is not None and self.mask is not None:
+            return ARTAttack.to_unified_format(self.attack.generate(
+                x=self.data.x, y=self.data.y, mask=self.mask, reset_patch=self.reset_patch))
         return ARTAttack.to_unified_format(self.attack.generate(x=data, reset_patch=self.reset_patch))
+
+    def set_data(self, data):
+        self.data = data
 
     def set_classifier(self, model):
         if isinstance(model, keras.Model):
-            self.classifier = KerasClassifier(model=model, clip_values=(min_pixel_value, max_pixel_value))
-            self.classifier.fit(x_train, y_train, batch_size=64, nb_epochs=3)
+            # Setting classifier for Keras model, todo adding optional parameters
+            self.classifier = KerasClassifier(model=model)
+        elif isinstance(model, torch.nn.Module):
+            # Pytorch model
+            if self.input_shape is None or self.loss is None or self.nb_classes is None:
+                raise Exception("PyTorch model needs input_shape, lass and nb_classes to conduct attack")
+            self.classifier = PyTorchClassifier(
+                model=model, input_shape=self.input_shape, loss=self.loss, nb_classes=self.nb_classes)
+        elif isinstance(model, sklearn.base.BaseEstimator):
+            # todo adding optional parameters for sklearn model classifier
+            self.classifier = SklearnClassifier(model=model)
         else:
+            # TODO: Implementation of TensorFlow (and V2) classifiers, BlackBox
             raise Exception("Model type not supported")
 
 
 class AdversarialPatch(ARTAttack):
-    # rotation_max (float) – The maximum rotation applied to random patches.
-    # The value is expected to be in the range [0, 180].
-
-    # scale_min (float) – The minimum scaling applied to random patches.
-    # The value should be in the range [0, 1], but less than scale_max.
-
-    # scale_max (float) – The maximum scaling applied to random patches.
-    # The value should be in the range [0, 1], but larger than scale_min.
-
-    # learning_rate (float) – The learning rate of the optimization.
-
-    # max_iter (int) – The number of optimization steps.
-
-    # batch_size (int) – The size of the training batch.
-
-    # patch_shape – The shape of the adversarial patch as a tuple of shape (width, height, nb_channels).
-    # Currently only supported for TensorFlowV2Classifier. For classifiers of other frameworks the patch_shape
-    # is set to the shape of the input samples.
-
-    # targeted (bool) – Indicates whether the attack is targeted (True) or untargeted (False).
-
-    # verbose (bool) – Show progress bars.
-
     def __init__(self, args):
         super().__init__(args)
 
+        # rotation_max (float) – The maximum rotation applied to random patches.
+        # The value is expected to be in the range [0, 180].
         if "rotation_max" in args:
             self.rotation_max = args["rotation_max"]
         else:
             self.rotation_max = 22.5
 
+        # scale_min (float) – The minimum scaling applied to random patches.
+        # The value should be in the range [0, 1], but less than scale_max.
         if "scale_min" in args:
             self.scale_min = args["scale_min"]
         else:
@@ -109,31 +107,39 @@ class AdversarialPatch(ARTAttack):
         else:
             self.scale_max = 1.0
 
+        # learning_rate (float) – The learning rate of the optimization.
         if "learning_rate" in args:
             self.learning_rate = args["learning_rate"]
         else:
             self.learning_rate = 5.0
 
+        # max_iter (int) – The number of optimization steps.
         if "max_iter" in args:
             self.max_iter = args["max_iter"]
         else:
             self.max_iter = 500
 
+        # batch_size (int) – The size of the training batch.
         if "batch_size" in args:
             self.batch_size = args["batch_size"]
         else:
             self.batch_size = 16
 
+        # patch_shape – The shape of the adversarial patch as a tuple of shape (width, height, nb_channels).
+        # Currently only supported for TensorFlowV2Classifier. For classifiers of other frameworks the patch_shape
+        # is set to the shape of the input samples.
         if "patch_shape" in args:
             self.patch_shape = args["patch_shape"]
         else:
             self.patch_shape = None
 
+        # targeted (bool) – Indicates whether the attack is targeted (True) or untargeted (False).
         if "targeted" in args:
             self.targeted = args["targeted"]
         else:
             self.targeted = True
 
+        # verbose (bool) – Show progress bars.
         if "verbose" in args:
             self.verbose = args["verbose"]
         else:
