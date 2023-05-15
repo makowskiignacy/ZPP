612337f6f18768aba04315691cf73895ff51e0e3
Ignacy Makowski <i.makowski@student.uw.edu.pl>
2023-05-15 15:31:21 +0200

    module description

--- tests/other_tests/test_cloud.py
+++ tests/other_tests/test_cloud.py
@@ -1,15 +1,11 @@
 from cloud_sync import get_model
 import numpy as np
 
-from attacks.artattacks.fast_gradient import FastGradient as SampleArtAttack
-from attacks.foolboxattacks.basic_iterative import L1BasicIterative as SampleFoolboxAttack
-from attacks.helpers.parameters import ARTParameters, FoolboxParameters
-from attacks.helpers.data import Data
-
 EXPERIMENT_NAME = '01339e26-a4b9-4d4b-b999-fd598a59576a'
 RUN_NAME = 'skam_search'
 
 def test_model_usage():
+    # An example of using a model downloaded from MLPlatform
     model = get_model(EXPERIMENT_NAME, RUN_NAME, './TEMPORARY')
     input_example = np.array(np.random.rand(71), dtype=np.float32)
     print(model.predict(input_example))
