ce04341016e0f5760491db48640e2aea3cfb3506
Grzegorz Żuk <grzuk7@gmail.com>
2023-06-15 23:12:04 +0200

    added binary models fix for cloud

--- tests/input_loader.py
+++ tests/input_loader.py
@@ -113,7 +113,6 @@ def simple_input(batchsize=4):
 
     images, labels = fb.utils.samples(foolbox_model, dataset='imagenet', batchsize=batchsize)
     foolbox_data = Data(images, labels)
-    test_logger.debug(f"############################ simple res labels: {numpy.shape(labels)} {labels}")
     art_data = Data(images.numpy(), labels.numpy())
     art_model = art.estimators.classification.pytorch.PyTorchClassifier(model=model, input_shape=(3, 224, 224),
                                                                         loss=nn.CrossEntropyLoss(),
@@ -244,9 +243,7 @@ def nn_input():
     result = torch.tensor(result, requires_grad=False, dtype=torch.float)
     foolbox_data = Data(data, result)
 
-
-    if  is_binary_model(result):
-        BaseGradientDescent.get_loss_fn = custom_get_loss_fn
+    BaseGradientDescent.get_loss_fn = custom_get_loss_fn
 
     art_data = Data(data.numpy(), result.numpy())
     art_model = art.estimators.classification.pytorch.PyTorchClassifier(model=nn_model,
@@ -313,7 +310,11 @@ def nn_input():
 
 
 def nn_input_cloud():
-    ss_nn_pipeline = mlflow.sklearn.load_model("../ss_nn/")
+    cwd = os.getcwd()
+    model_path = os.path.join(cwd, "ss_nn/")
+    data_path = os.path.join(cwd, 'data_test.csv')
+
+    ss_nn_pipeline = mlflow.sklearn.load_model(model_path)
     if ss_nn_pipeline is not None:
         nn_model = ss_nn_pipeline.steps[1][1].module_
         fmodel = fb.models.pytorch.PyTorchModel(nn_model, bounds=(-2., 30000.))
@@ -353,6 +354,10 @@ def nn_input_cloud():
         attack_parameters_default = {}
         art_parameters_default = ARTParameters(classifier_parameters_default, attack_parameters_default)
 
+        generic_parameters_an = {"epsilon": 0.01}
+        attack_specific_parameters_an = {}
+        foolbox_parameters_an = FoolboxParameters(attack_specific_parameters_an, generic_parameters_an)
+
         generic_parameters_bb = {"epsilon_rate": 0.01}
         attack_specific_parameters_bb = {"lr": 10, 'steps': 100}
         foolbox_parameters_bb = FoolboxParameters(attack_specific_parameters_bb, generic_parameters_bb)
@@ -360,6 +365,14 @@ def nn_input_cloud():
         generic_parameters_bi = {"epsilon_rate": 0.05}
         attack_specific_parameters_bi = {"steps": 100, "random_start": True}
         foolbox_parameters_bi = FoolboxParameters(attack_specific_parameters_bi, generic_parameters_bi)
+
+        generic_parameters_cw = {"epsilon_rate": 0.01}
+        attack_specific_parameters_cw = {"steps": 100}
+        foolbox_parameters_cw = FoolboxParameters(attack_specific_parameters_cw, generic_parameters_cw)
+
+        generic_parameters_nf = {"epsilon_rate": 0.01}
+        attack_specific_parameters_nf = {"steps": 100, "stepsize": 100}
+        foolbox_parameters_nf = FoolboxParameters(attack_specific_parameters_nf, generic_parameters_nf)
 
         generic_parameters_pgd = {"epsilon_rate": 0.01}
         attack_specific_parameters_pgd = {"steps": 100, "random_start": True}
@@ -369,10 +382,13 @@ def nn_input_cloud():
         attack_specific_parameters_sap = {"steps": 100, "across_channels": True}
         foolbox_parameters_sap = FoolboxParameters(attack_specific_parameters_sap, generic_parameters_sap)
 
-        foolbox_parameters = {"brendel_bethge": foolbox_parameters_bb,
-                              "basic_iterative": foolbox_parameters_bi,
-                              "projected_gradient_descent": foolbox_parameters_pgd,
-                              "salt_and_pepper": foolbox_parameters_sap}
+        foolbox_parameters = {"additive_noise": foolbox_parameters_an,
+                            "brendel_bethge": foolbox_parameters_bb,
+                            "basic_iterative": foolbox_parameters_bi,
+                            "carlini_wagner": foolbox_parameters_cw,
+                            "newton_fool": foolbox_parameters_nf,
+                            "projected_gradient_descent": foolbox_parameters_pgd,
+                            "salt_and_pepper": foolbox_parameters_sap}
 
         art_parameters = {"deep_fool": art_parameters_default,
                           "fast_gradient": art_parameters_default,
@@ -384,6 +400,8 @@ def nn_input_cloud():
                           "square": art_parameters_default,
                           "threshold": art_parameters_default,
                           "zeroth_order_optimization": art_parameters_default}
+    
+        BaseGradientDescent.get_loss_fn = custom_get_loss_fn
 
         return fmodel, nn_model, foolbox_data, art_data, foolbox_parameters, art_parameters
     return None, None
