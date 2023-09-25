e3e7507074b73afe313570af2123f896f7a40370
Hubert Lubański <73351095+AxeVanced@users.noreply.github.com>
2023-09-25 17:33:42 +0200

    Update documentation.py

--- documentation.py
+++ documentation.py
@@ -1,13 +1,19 @@
+# Przykładowe użycie dokumentacji wewnątrz Pythona
 from attacks import *
 from attacks.art_attack import *
 from attack_manager import *
 
+# Dokumentacja klasy abstrakcyjnej ataku
 help(Attack)
+
+# oraz jej podklasy specyficznej dla biblioteki
 help(ARTAttack)
+
+# Dokumentacja jednego z ataków z biblioteki ART
 help(AdversarialPatch)
+
+# Dokumentacja jednego z ataków z biblioteki Foolbox
 help(DeepFool)
-help(FastGradient)
-help(GeometricDecisionBased)
+
+# Dokumentacja Attack Managera
 help(AttackManager)
-help(AttackListEntry)
-help(AttackNameNotRecognized)
